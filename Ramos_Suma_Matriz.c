c560de18b0d40b3bd72b358e4459c6ef590d379e
Jorge <jorge.ramos.vasquez@udla.edu.ec>
2023-11-28 15:01:47 -0500

    Ingreso de los datos del usuario

--- Ramos_Suma_Matriz.c
+++ Ramos_Suma_Matriz.c
@@ -6,3 +6,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+int main()
+{
+    // ingreso y reconocimiento de variables y generacion del codigo
+    int n, m;
+    // Escaneamos la dimension de la matriz que ingrese el usuario
+    printf("Ingrese el valor para filas: ");
+    scanf("%d", &n);
+    printf("Ingrese el valor para columnas: ");
+    scanf("%d", &m);
