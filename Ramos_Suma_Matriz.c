841f56ee4ae33abfea33bd9ac566d30ce3256f83
Jorge <jorge.ramos.vasquez@udla.edu.ec>
2023-11-28 15:06:05 -0500

    Ingreso de las matrices que se van a sumar

--- Ramos_Suma_Matriz.c
+++ Ramos_Suma_Matriz.c
@@ -1,7 +1,7 @@
 // Suma de matrices
 // Usuario Ingrese dimenciones de la matriz
 // #aleatorios entre 0 y 100
-// Imprima las matrices 
+// Imprima las matrices
 // Colocamos las bibliotecas que se utilizaran para el codigo
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,3 +15,29 @@ int main()
     scanf("%d", &n);
     printf("Ingrese el valor para columnas: ");
     scanf("%d", &m);
+    // Agregamos la matriz con los numeros aliatorios y la imrpimimos 
+    int matrix1[n][m];
+    printf("La primera matriz es: \n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("\n");
+        for (int j = 0; j < m; j++)
+        {
+            matrix1[i][j] = rand() % 101;
+            ;
+            printf("%d\t", matrix1[i][j]);
+        }
+    }
+    // Colocamos la segunada matriz con numeros aleatorios que se van a sumar y la imprimimos
+    int matrix2[m][n];
+    printf("\n");
+    printf("\nLa segunda matriz es: \n");
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            matrix2[i][j] = rand() % 101;
+            printf("%d\t", matrix2[i][j]);
+        }
+        printf("\n");
+    }
